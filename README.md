865cd2aa01fbe5cd22d0e5e53d2a824f2d50005c
Adam David Scott <adam@adamscottphd.com>
2015-11-23 14:41:47 -0600

    added ClinicalTrials and VEP

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # restAPIs
 Integrating ReST APIs through a more common framework.
+## Variant Effect Predictor
+Based on [Ensembl's VEP annotator](http://rest.ensembl.org/#Variation)
+## Clinical Trials
+Based on [ClinicalTrials.gov](https://clinicaltrials.gov/ct2/info/linking)
---
+++ main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# author: Adam D Scott (amviot@gmail.com)
+# first created: 2015*09*28
+
+import sys
+import getopt
+import requests
+import json
+import tempfile
+from restAPI import restAPI
+from requests.auth import HTTPDigestAuth
+from xmlutils.xml2json import xml2json
+import xml.etree.ElementTree as ET
+
+def parseArgs( argv ):
+	helpText = "python main.py" + "\n\tRequired input:\n"
+	helpText += "\t\t-u <username> -k <key>\tOR\t-c <API credentials file: user \\n key>" + "\n"
+	helpText += "\tOptional inputs:\n"
+	helpText += "\t\t-o <outputfile>\n"# -a <REST API site>\n"
+	username = ""
+	password = ""
+	api = ""
+	output = ""
+	try:
+		opts, args = getopt.getopt( argv , "hc:a:u:k:o:" , ["cred=" , "api=" , "user=" , "key=" , "output="] )
+	except getopt.GetoptError:
+		print "ADSERROR: Command not recognized"
+		print( helpText ) 
+		sys.exit(2)
+	if not opts:
+		print "ADSERROR: Expected flagged input"
+		print( helpText ) 
+		sys.exit(2)
+	for opt, arg in opts:
+		print opt + " " + arg
+		if opt in ( "-h" , "--help" ):
+			print( helpText )
+			sys.exit()
+		elif opt in ( "-c" , "--cred" ):
+			[username,password] = list( open( arg ) )
+		elif opt in ( "-a" , "--api" ):
+			api = arg
+		elif opt in ( "-u" , "--user" ):
+			username = arg
+		elif opt in ( "-k" , "--key" ):
+			password = arg
+		elif opt in ( "-o" , "--output" ):
+			output = arg
+	return { "username" : username , "password" : password , "api" : api , "output" : output }
+	
+def checkConnection():
+	search = "https://"
+	res = requests.get( search )
+	print search
+	if res:
+		print "have xml"
+	else:
+		print res.status_code
+
+def main( argv ):
+	values = parseArgs( argv )
+	username = values["username"].strip()
+	password = values["password"].strip()
+	output = values["output"]
+
+	search = ""
+	res = requests.get( search , auth=HTTPDigestAuth( username , password ) )
+	if res:
+		records = ""
+		if records:
+			print "records obtained: " + str(len(records))
+			print records
+	else:
+		print res.status_code
+	print search
+
+if __name__ == "__main__":
+	main( sys.argv[1:] )
